0362a804fb167127b611c8509e1665314c5ca747
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2025-09-28 23:00:36 +0800

    feat: add validation checking maximum possible length of the argument

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -6,7 +6,7 @@ from src.exceptions.validation import ArgumentException, OperationException
 
 class Validator:
     @staticmethod
-    def validate(value: Any, expected_type: object, length: int = None) -> bool:
+    def validate(value: Any, expected_type: object, length: int | None = None, max_length: int | None = None) -> bool:
         #if value is None:
             #raise ArgumentException('Empty argument')
         
@@ -21,6 +21,10 @@ class Validator:
 
         if length is not None and len(str(value).strip()) != length:
             raise ArgumentException(f'Incorrect length of argument. The length of this argument should be {length}')
+        
+
+        if max_length is not None and len(str(value).strip()) > max_length:
+            raise ArgumentException(f'Incorrect length of argument. The length of this argument should be less of {max_length}')
         
 
         return True
