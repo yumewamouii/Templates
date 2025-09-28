de486f6b628eb66ff5208377c7e10882d394a90f
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2025-09-28 23:02:33 +0800

    feat: add model nomenclature

--- src/models/nomeclature.py
+++ src/models/nomeclature.py
@@ -1,6 +1,10 @@
 from src.models.base_model import BaseModel
+from src.models.nomenclature_group import NomenclatureGroup
+from src.models.measurement_unit import MeasurementUnit
 from src.utils.fields import ValidatedField
 
 
 class Nomenclature(BaseModel):
-    pass
+    fullname = ValidatedField(str, max_length = 255, strip = True, nullable = False, blank = False)
+    nomenclature_group = ValidatedField(NomenclatureGroup, nullable = False, blank = False)
+    measurement_unit = ValidatedField(MeasurementUnit, nullable = False, blank = False)
