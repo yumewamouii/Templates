dec60f9ee7e9e75a846b0bef2386d22feaea7c47
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2025-09-28 23:07:49 +0800

    feat: add validation of max length

--- src/models/storage.py
+++ src/models/storage.py
@@ -3,5 +3,5 @@ from src.utils.fields import ValidatedField
 
 
 class Storage(BaseModel):
-    id = ValidatedField(str, strip = True, nullable = False, blank = False)
+    id = ValidatedField(str, strip = True, max_length = 50, nullable = False, blank = False)
     name = ValidatedField(str, strip = True, nullable = False, blank = False, default = 'Storage')
