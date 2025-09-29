7ea11879ba9a3e519bf4bcce3a7e3908a7ac2e85
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2025-09-29 12:09:20 +0800

    Revert "add model MeasurementUnit"
    This reverts commit 277fb140dc11904ff8bb6ea1cc0d83a2dfcd1c90.

--- src/models/measurement_unit.py
+++ src/models/measurement_unit.py
@@ -1,35 +1,6 @@
-from __future__ import annotations
-import uuid
-
-
-
 from src.models.base_model import BaseModel
 from src.utils.fields import ValidatedField
 
 
 class MeasurementUnit(BaseModel):
-    basic_unit = ValidatedField(str, strip = True, nullable = True, max_length = 50)
-    conversion_factor = ValidatedField(int, nullable = False, blank = False, default = 1)
-
-
-    @classmethod
-    def range_model(cls, name: str, factor: int = 1, base_unit: 'MeasurementUnit' | None = None) -> 'MeasurementUnit':
-        unit = cls()
-        unit.basic_unit = name
-
-
-        if base_unit is None:
-            unit.conversion_factor = 1
-        else:
-            unit.conversion_factor = factor
-            unit.basic_unit = base_unit.basic_unit
-
-
-        return unit
-
-    def __str__(self):
-        return (f'Идентификационный номер: {self.id}\n'
-                f'Базовая единица измерения: {self.basic_unit}\n'
-                f'Коэффициент пересчета: {self.conversion_factor}')
-
-
+    pass
