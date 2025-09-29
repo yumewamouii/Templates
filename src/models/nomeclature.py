df1dd44af93cb8e0fca78c5cc4ed3d616dcb3d37
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2025-09-29 12:08:56 +0800

    Revert "fix: little fix with wrong naming"
    This reverts commit 3ba4f798921b09596fce20b7ad9922fc4e7d351f.

--- src/models/nomeclature.py
+++ src/models/nomeclature.py
@@ -5,10 +5,6 @@ from src.utils.fields import ValidatedField
 
 
 class Nomenclature(BaseModel):
-    fullname = ValidatedField(str, max_length = 255, strip = True, nullable = False, blank = False, default = 'nomeclaturefullname')
+    fullname = ValidatedField(str, max_length = 255, strip = True, nullable = False, blank = False)
     nomenclature_group = ValidatedField(NomenclatureGroup, nullable = False, blank = False)
     measurement_unit = ValidatedField(MeasurementUnit, nullable = False, blank = False)
-
-
-    def __str__(self):
-        return (f'Fullname is {self.fullname}')
