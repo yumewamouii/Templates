aa8b23ea9f5cc485494db56653827f853a079bbf
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2025-09-29 12:08:19 +0800

    Revert "style: fix naming"
    This reverts commit 500c6542f1be63511edb78cb69c6287299387e5d.

--- test/test_settings.py
+++ test/test_settings.py
@@ -9,25 +9,12 @@ class TestModelSettings(unittest.TestCase):
     def setUp(self):
         self.settings = Settings()
         self.settings_first = Settings()
-        self.settings_second = Settings()
+        self.settings_two = Settings()
     
 
     def test_create_settings(self):
         self.assertIsInstance(self.settings, Settings)
     
 
-    def test_not_equal_settings(self):
-        self.assertNotEqual(self.settings_first, self.settings_second)
-    
-
-    def test_not_equal_ids_settings(self):
-        self.assertNotEqual(self.settings_first.company.id, self.settings_second.company.id)
-    
-
-    def test_equal_properties_without_id_settings(self):
-        self.assertEqual((self.settings_first.company.name, self.settings_first.company.tin, self.settings_first.company.account,
-                          self.settings_first.company.corr_account, self.settings_first.company.bic, 
-                          self.settings_first.company.ownership_type),
-                          (self.settings_second.company.name, self.settings_second.company.tin, self.settings_second.company.account,
-                          self.settings_second.company.corr_account, self.settings_second.company.bic, 
-                          self.settings_second.company.ownership_type))
+    def test_equal_settings(self):
+        pass
