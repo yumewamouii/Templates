f642a7f2e79d0b32d1256e29659d109be40e42a9
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2025-09-29 12:10:23 +0800

    Revert "feat: create storage model"
    This reverts commit b13073429f84fc1fa368f3f287e13d5ddf52e9a1.

--- src/models/storage.py
+++
@@ -1,7 +0,0 @@
-from src.models.base_model import BaseModel
-from src.utils.fields import ValidatedField
-
-
-class Storage(BaseModel):
-    id = ValidatedField(str, strip = True, nullable = False, blank = False)
-    name = ValidatedField(str, strip = True, nullable = False, blank = False, default = 'Storage')
